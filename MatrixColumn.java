fef1786f3bd21ac45b0ffc9f75a0435949afd481
Patrick.Westgaard <36091197+RobAnybody413@users.noreply.github.com>
2018-03-07 18:49:43 -0800

    Update MatrixColumn.java

--- MatrixColumn.java
+++ MatrixColumn.java
@@ -1,4 +1,3 @@
-
 public class MatrixColumn implements Node, HeadNode{
     private Node nextInRow;
     private Node nextInColumn;
@@ -28,11 +27,60 @@ public class MatrixColumn implements Node, HeadNode{
     }
 
     public void insert(ValueNode value) {
+    	MatrixColumn att = new MatrixColumn();
+    	HeadNode attainHead = att;
+    	for (int i = 1; i < value.getColumn(); i++) {
+    		attainHead = attainHead.getNext();
+    	} //gets us to the right column head
+    	ValueNode column = attainHead.getFirst(); // makes a new node at column head to iterate
+    	ValueNode getTheRow = (ValueNode)column.getNextInRow();
+	while (column.getRow() > getTheRow.getRow()) { //compare the row this node want to be in with the row the other node is in
+			column = (ValueNode)column.getNextInRow();
+			getTheRow = (ValueNode)getTheRow.getNextInRow();
+		}
+		if (column.getNextInRow() != null) {
+			value.setNextInRow(column.getNextInRow());
+		}
+		column.setNextInRow(value); 
         //insert along nextInColumn
+		@Override
+		public void setNextInColumn(ValueNode valuenode) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setNextInRow(ValueNode vlasdljg) {
+			
+			// TODO Auto-generated method stub
     }
 
     public int get(int position) {
-        return 0;
+    	ValueNode cur = (ValueNode)nextInColumn;
+
+        while(cur != null && cur.getRow() < position) {
+            cur = (ValueNode)cur.getNextInColumn();
+        }
+
+        if(cur == null || cur.getRow() > position)
+            return 0;
+        else
+            return cur.getValue();
     }
 
+
+	public Node getNextInCollumn() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public void setNextInCollumn(Node next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
+
+	
